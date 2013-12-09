9d3784e85f257adfe8cdc7280ca0c8f49236c1f6
Yani0029 <yani@bellcom.dk>
2013-12-09 10:40:12 +0100

    Change background pictures pathes

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ Drupal.behaviors.camping = {
       $('#main-wrapper #content').removeClass('span12');
       $('#main-wrapper #content').addClass('span8');
     }
-    $('a.hytter').parents("li").css("background","url('/sites/all/themes/Camping/images/house.png') 0 center no-repeat #9F2A68");
-    $('a.autocamper').parents("li").css("background","url('/sites/all/themes/Camping/images/autocamp.png') 0 center no-repeat #9DB330");
+    $('a.hytter').parents("li").css("background","url('/sites/all/themes/Camping-theme/images/house.png') 0 center no-repeat #9F2A68");
+    $('a.autocamper').parents("li").css("background","url('/sites/all/themes/Camping-theme/images/autocamp.png') 0 center no-repeat #9DB330");
 
     $('.booking-result').click(function(e){
       $(this).css({'display':'block','background-color': 'red','width':'10px','height': "10px",});
